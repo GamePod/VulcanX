38f732ac8b8ced15add9f8dbf619ff0cbe455286
GamePod <47813418+GamePod@users.noreply.github.com>
2019-02-22 16:30:40 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+
+"platform": "云主机-控制台",
+"sync_transaction_version": "17",
+"type": "url",
+"username":"测试主机",
+"qcloudAppId: '1013760523',
+"qcloudSecretId: 'AKIDc4Q483nNnMtKcnwtpbq892jusxW8tBYQ',
+"qcloudSecretKey: 'Jj1CDSkwRbaABC7aYO9sloiS62wiLwNd',
+"url": "https://console.cloud.tencent.com/cvm/index"
+}, {
+"date_added": "13170737011558579",
+"id": "13",
